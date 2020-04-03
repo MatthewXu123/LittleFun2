2b33874294085855d2ed2869dfd82b79138bee06
Matthew Xu <matthew.xu@carel.locale>
2020-04-03 16:45:19 +0800

    Initial commit

---
+++ src/main/java/com/matthewxu/LittleFunApplication.java
@@ -0,0 +1,13 @@
+package com.matthewxu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleFunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleFunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matthewxu/LittleFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewxu;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
