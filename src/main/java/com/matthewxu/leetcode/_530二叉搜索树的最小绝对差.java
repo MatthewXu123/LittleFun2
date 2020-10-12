c1ab094bf943a1b00dcf4d35a3cd9c404369c965
Matthew Xu <matthew.xu@carel.locale>
2020-10-12 09:45:43 +0800

    Update _530二叉搜索树的最小绝对差.java

--- src/main/java/com/matthewxu/leetcode/_530二叉搜索树的最小绝对差.java
+++ src/main/java/com/matthewxu/leetcode/_530二叉搜索树的最小绝对差.java
@@ -1,51 +1,55 @@
 package com.matthewxu.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class _530二叉搜索树的最小绝对差 {
 
-	public static int getMinimumDifference(TreeNode root) {
-		int diff = -1;
-		List<Integer> list = new ArrayList<>();
-		while (root != null) {
-			list.add(root.val);
-			root = root.right;
-		}
-		for(int i = 0; i < list.size(); i++){
-			for(int j = i + 1; j < list.size(); j++){
-				
-				int tempDiff = (list.get(i) == null ? 0 : list.get(i)) - (list.get(j) == null ? 0 : list.get(j));
-				if (tempDiff <= 0)
-					tempDiff = tempDiff * -1;
-				if (diff == -1 || tempDiff < diff)
-					diff = tempDiff;
-			}
-		}
-		return diff;
-	}
+	static int pre;
+    static int min;
+
+    public static int getMinimumDifference(TreeNode root) {
+        pre = -1;
+        min = Integer.MAX_VALUE;
+        dfs(root);
+        return min;
+    }
+    
+    public static void dfs(TreeNode node){
+    	if(node == null)
+    		return;
+    	dfs(node.left);
+    	if(pre == -1)
+    		pre = node.val;
+    	else{
+    		min = Math.min(min, node.val - pre);
+    		pre = node.val;
+    	}
+    	dfs(node.right);
+    	
+    }
 
 	static class TreeNode {
 		int val;
 		TreeNode left;
 		TreeNode right;
-
 		TreeNode(int x) {
 			val = x;
 		}
 	}
 
 	public static void main(String[] args) {
+		//[1, null, 2, 4]
+//		 1
+//	      \
+//	       2
+//	      /
+//	     4
 		TreeNode node1 = new TreeNode(1);
-		TreeNode node2 = null;
-		TreeNode node3 = new TreeNode(4);
+		TreeNode node2 = new TreeNode(4);
+		TreeNode node3 = new TreeNode(2);
 
 		node1.left = null;
 		node1.right = node2;
-//		node2.left = node1;
-//		node2.right = node3;
-		node3.left = node2;
-		node3.right = null;
+		node2.left = node3;
+		node2.right = null;
 		System.out.println(getMinimumDifference(node1));
 	}
 }
