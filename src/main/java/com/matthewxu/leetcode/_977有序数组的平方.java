44a3c6d9b4e5ca2a764c14b49e642f56a44a5bc0
Matthew Xu <MatthewXu123@163.com>
2020-10-16 07:14:22 +0800

    1016

---
+++ src/main/java/com/matthewxu/leetcode/_977有序数组的平方.java
@@ -0,0 +1,20 @@
+package com.matthewxu.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class _977有序数组的平方 {
+
+	public static int[] sortedSquares(int[] A) {
+		List<Integer> list = new ArrayList<>();
+		Arrays.stream(A).forEach(ele -> list.add(ele * ele));
+		Collections.sort(list);
+		for(int i = 0; i < list.size();i++){
+			A[i] = list.get(i);
+		}
+		return A;
+    }
+	
+}
