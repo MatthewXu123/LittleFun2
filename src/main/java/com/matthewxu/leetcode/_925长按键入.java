6a165491cf190225c5c233f83311c8abf6aa1e9e
Matthew Xu <matthew.xu@carel.locale>
2020-10-21 15:53:21 +0800

    Update _925长按键入.java

--- src/main/java/com/matthewxu/leetcode/_925长按键入.java
+++ src/main/java/com/matthewxu/leetcode/_925长按键入.java
@@ -9,6 +9,17 @@ package com.matthewxu.leetcode;
 public class _925长按键入 {
 
 	public boolean isLongPressedName(String name, String typed) {
-		
+        int i = 0, j = 0;
+        while (j < typed.length()) {
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+                i++;
+                j++;
+            } else if (j > 0 && typed.charAt(j) == typed.charAt(j - 1)) {
+                j++;
+            } else {
+                return false;
+            }
+        }
+        return i == name.length();
     }
 }
