2a8ade04dfe69dcb5743196ab9eac13b0cbea6dd
Matthew Xu <matthew.xu@carel.locale>
2020-10-21 14:45:53 +0800

    Create _925长按键入.java

---
+++ src/main/java/com/matthewxu/leetcode/_925长按键入.java
@@ -0,0 +1,14 @@
+
+package com.matthewxu.leetcode;
+
+/**
+ * Description:
+ * @author Matthew Xu
+ * @date Oct 21, 2020
+ */
+public class _925长按键入 {
+
+	public boolean isLongPressedName(String name, String typed) {
+		
+    }
+}
